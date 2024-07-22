dc68a44630ae51e195580285d1d702f86b2fd138
sumeyye elgin <sumeyyeelgin@hotmail.com>
2024-07-22 14:34:11 +0300

    app fonksiyonuna defalt anahtar kelimesi eklendi

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export function App() {
+export default function App() {
   return (
     <View>
       <Text>Hello world!!!</Text>
