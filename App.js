fd887d047e2308c0aa0cb92b3e0e0dc20d521d6d
sumeyye elgin <sumeyyeelgin@hotmail.com>
2024-07-22 15:58:46 +0300

    components yapıları derslerinde yazılan kodlar

--- App.js
+++ App.js
@@ -20,12 +20,33 @@ const styles = StyleSheet.create({
 });*/
 
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  return (
+  function sayHello() {
+    console.log("hello world")
+  }
+  // componentler bir tane component döndürmeli yani return'un içerisinde bir tane ana parent bulunmalı, react'ın bir kuralıdır
+  return ( 
     <View>
-      <Text>Hello world!!!</Text>
+      <View>
+        <Text>Hello world!!!</Text> 
+        <Text>Hello world</Text>
+      </View>
+      {/* buttondaki title ve onPress zounlu proplar, onPress fonskiyon alır */}
+      <Button title='Here press me!' onPress={() => {console.log(sayHello())}} color={'mistyrose'} />
     </View>
   );
 }
+
+// arrow functionla kurulmuş hali
+// const App = () => {
+//   return ( 
+//     <View>
+//       <View>
+//         <Text>Hello world!!!</Text> 
+//         <Text>Hello world</Text>
+//       </View>
+//     </View>
+//   );
+// }
